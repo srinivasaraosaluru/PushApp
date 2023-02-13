987c0e8cc2d9644312eec04a41ba473e11fc9bbc
srinivasaraosaluru <saluru.srinivasa.rao@gds.ey.com>
2023-02-13 15:59:53 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lla.testpush',
+  appName: 'PushApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
