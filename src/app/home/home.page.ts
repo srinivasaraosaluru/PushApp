3d8149685d6e4d565f65cc9baf1de2039c7063f6
srinivasaraosaluru <saluru.srinivasa.rao@gds.ey.com>
2023-02-15 17:38:06 +0530

    Ionic Push App Initial Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
 
+import {  EmarsysSDKCustom } from  'capacitor-emarsys-sdk-plugin'
+
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  // public contacts: Contacts1[] = [];
+  public value!: string;
   constructor() {}
+
+  async getUUID(){
+    this.value =  (await EmarsysSDKCustom.getUUID('test')).value;
+    console.log("My UUID", this.value);
+
+    EmarsysSDKCustom.addListener('pushMessageEvent', (message: any) => {
+      // do something
+      alert("asdsad");
+      console.log("sdsadsad");
+    });
+  }
+
+  async addListener(){
+    EmarsysSDKCustom.addListener('pushMessageEvent', (message: any) => {
+      // do something
+      alert("asdsad");
+      console.log("Testing.....");
+    });
+  }
 
 }
